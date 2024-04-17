170f5426b3d54769f2d90d9abce43cafaf5eacc2
saurav470 <sauravsharma3001@gmail.com>
2024-04-17 21:42:18 +0530

    job add

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@ const fn: () => void = () => {
 }
 
 fn()
+
+
+const obj={
+  name:"shahbaz",
+  age:22,
+  city:"india",
+  city1:[1,2,3,3]
+}
+
+
+
+console.log(obj["age"],obj.age,Object.keys(obj),Object.values(obj))
